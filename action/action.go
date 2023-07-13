a27e3b8d1db7f9215117809830de1b9b0a0bb4c0
qilvge <2571436563@qq.com>
2023-07-13 22:55:40 +0800

    feat(新增调试方式):

--- action/action.go
+++ action/action.go
@@ -23,7 +23,65 @@ func Generator(_ *cobra.Command, _ []string) error {
 	if err != nil {
 		return err
 	}
+
+	//读取调试文件内容===========================开始===============
+	/*	file, err := os.Open("./demo.json")
+		if err != nil {
+			return err
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				fmt.Println("err=", err)
+			}
+		}(file)
+
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			return err
+		}
+
+		var result plugin.Plugin
+		err = json.Unmarshal(data, &result)
+		if err != nil {
+			return err
+		}
+		p := &result*/
+	//读取调试文件内容===========================结束===============
+
 	basepath := VarStringBasePath
 	host := VarStringHost
 	return generate.Do(fileName, host, basepath, p)
+}
+
+func Demo() {
+	//新增插入JSON格式文件======================开始=================
+	//debug命令：
+	//api plugin -p goctl-swagger="swagger -filename gateway.json" --api="./gateway.api" --dir .
+
+	//插入代码路径
+	//tools/goctl/plugin/plugin.go
+	//content, err := execx.Run(bin+" "+args, filepath.Dir(ex), bytes.NewBuffer(transferData)) 代码前面插入
+	/*	file, err := os.Create("demo.json")
+		if err != nil {
+			return err
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				fmt.Println("文件打开错误")
+			}
+		}(file)
+
+		_, err = file.Write(transferData)
+		if err != nil {
+			// 处理写入文件错误
+			return err
+		}
+
+		err = file.Sync()
+		if err != nil {
+			return err
+		}*/
+	//新增插入JSON格式文件======================结束=================
 }
